4e82c3314bfdaecd4c30698eaa7e33364b6c57f8
Zongkai LI <onionpiece@163.com>
2020-06-23 10:28:17 +0800

    Revert "Add connectivity to etcdclient"
    This reverts commit 2bdd2247dd2473e4d72d468769bb525d9bc8a814.

--- etcd.go
+++ etcd.go
@@ -69,21 +69,6 @@ func NewEtcdv3Client(caCertFile, certFile, keyFile, etcdEndpoints string) (*Etcd
 	if err != nil {
 		return nil, err
 	}
-	// test clientv3 connectivity
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	ops := []clientv3.OpOption{
-		clientv3.WithPrefix(),
-		clientv3.WithLimit(1),
-	}
-	resp, err := client.Get(ctx, "/", ops...)
-	if err != nil {
-		return nil, err
-	}
-	if len(resp.Kvs) == 0 {
-		return nil, nil
-	}
-
 	return &Etcdv3Client{Client: client}, nil
 }
 
